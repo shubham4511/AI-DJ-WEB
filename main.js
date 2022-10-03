894ae2aaf487f95fc0d876620653614d2b6c9e48
shubham4511 <89202701+shubham4511@users.noreply.github.com>
2022-10-03 18:20:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,84 +1,87 @@
-song="";
+peter_pen_song="";
+Harry_Potter_Theme_song="";
 leftWritsX=0;
 leftWritsY=0;
 rightWritsX=0;
 rightWritsY=0;
 scoreLeftWrits=0;
 scorerightWrits=0;
+song_peter_pen="";
+song_Harry_potter_Theme="";
+
 function setup(){
-    canvas=createCanvas(600,500);
+  canvas=createCanvas(600,530);
+  canvas.center();
+  video=createCapture(VIDEO);
+  video.hide();
+
+}
+function draw(){
+    image(video,0,0,600,530);
+    fill("red");
+    stroke("red");
+    song_peter_pen=peter_pen_song.isplaying;
+    console.log("Peter Pen Song="+song_peter_pen);
+
+    song_Harry_potter_Theme=Harry_Potter_Theme_song.isplaying;
+    console.log("Harry Potter Theme Song="+song_Harry_potter_Theme);
+
+
+    if(scorerightWrits>0.2){
+    circle(rightWritsX,rightWritsY,20);
+    Harry_Potter_Theme_song.stop();
+    if(song_peter_pen==false){
+      peter_pen_song.play();
+      document.getElementById("song_id"),innerHTML="Song Name:Peter Pen Song";
+    }
+     
+
+  }
+}
+if(scoreLeftWrits>0.2){
+  circle(leftWritsX,leftWritsY,20);
+  Harry_Potter_Theme_song.stop();
+  if(song_Harry_potter_Theme==false){
+    Harry_Potter_Theme_song.play();
+    document.getElementById("song_id"),innerHTML="Song Name:Harry Potter Theme song";
+  }
+  
+}
+function modelLoaded(){
+  console.log("PoseNet Is initialized");
+
+}
+function gotPoses(results){
+  if(results.length >0){
+    console.log(results);
+
+     scoreLeftWrits=results[0].pose.keypoints[9].score;
+     console.log("lrftWrits_score="+scoreLeftWrits);
+
+     scorerightWrits=results[0].pose.keypoints[10].score;
+     console.log("rightWrits_score="+scorerightWrits);
+
+     leftWrits_x=results[0].pose.leftWrits.x;
+     leftWrits_y=results[0].pose.leftWrits.y;
+     console.log("leftWrits_x="+leftWrits_x+"leftWrits_y="+leftWrits_y);
+
+     rightWrits_x=results[0].pose.rightWrits.x;
+     rightWrits_y=results[0].pose.rightWrits.y;
+     console.log("rightWrits_x="+rightWrits_x+"rightWrits_y="+rightWrits_y);
+  }
+}
+
+
+
+function preload(){
+    peter_pen_song=loadSound("music.mp3");
+    Harry_Potter_Theme_song=loadSound("music2.mp3");
+}
+function setup(){
+  canvas=createCanvas(600,500);
     canvas.center();
     video=createCapture(VIDEO)
     video.hide();
     poseNet=ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPoses);
 }
-function draw(){
-    image(video,0,0,600,500);
-    fill("red");
-    stroke("red");
-    if(scorerightWrits>0.2){
-    circle(rightWritsX,rightWritsY,20);
-    if(rightWritsY > 0 && rightWritsY <= 100){
-        document.getElementById("speed").innerHTML="speed=0.5";
-        song.rate(0.5); 
-    }
-    else if(rightWritsY > 100 && rightWritsY <= 200){
-        document.getElementById("speed").innerHTML="speed=1";
-        song.rate(1); 
-    }
-    else if(rightWritsY > 200 && rightWritsY <= 300 ){
-        document.getElementById("speed").innerHTML="speed=1.5";
-        song.rate(1.5); 
-    }
-    else if(rightWritsY > 300 && rightWritsY <= 400){
-        document.getElementById("speed").innerHTML="speed=2";
-        song.rate(2); 
-    }
-    else if(rightWritsY > 400 && rightWritsY <= 500){
-        document.getElementById("speed").innerHTML="speed=2.5";
-        song.rate(2.5); 
-    }
-}
-    if(scoreLeftWrits>0.2){
-    circle(leftWritsX,leftWritsY,20);
-    InNumber=Number(leftWritsY);
-    remove_decimals=floor(InNumber);
-    volume=remove_decimals/500;
-    document.getElementById("volume").innerHTML="Volume="+volume;
-    song.setVolume(volume);
-    }
-
-}
-function preload(){
-    song=loadSound("music.mp3");
-}
-function play(){
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
-
-
-}
-function modelLoaded(){
-    console.log('PoseNet Is Initized');
-}
-function gotPoses(results){
-    if(results.length>0){
-        console.log(results);
-        leftWritsX = results[0].pose.leftWrist.x;
-        leftWritsY = results[0].pose.leftWrist.y;
-        console.log(" leftWritsX= "+leftWritsX+" leftWritsY= "+leftWritsY);
-        rightWritsX = results[0].pose.rightWrist.x;
-         righttWritsY = results[0].pose.rightWrist.y;
-         console.log("rightWritsX="+rightWritsX+" rightWritsY= "+rightWritsY);
-         scoreLeftWrits=results[0].pose.keypoints[9].score;
-         console.log("scoreLeftWrits="+scoreLeftWrits);
-         scorerightWrits=results[0].pose.keypoints[10].score;
-         console.log("scorerightWrits="+scorerightWrits);
-
-
-
-    }
-}
-    
